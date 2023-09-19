f882d0922480d7ba16ccedee4782f49938d54ea1
Jose <jose.veliz@rappi.com>
2023-09-19 18:09:32 -0400

    Connect all microservices

--- src/apps/team-management/api/src/providers/connections.provider.ts
+++ src/apps/team-management/api/src/providers/connections.provider.ts
@@ -9,7 +9,7 @@ export const connections: Provider[] = [
     provide: 'DATABASE_CONNECTION',
     inject: ['DATABASE_CONFIGURATION'],
     useFactory: async (databaseConf: any) => {
-      const client = await MongoClient.connect(databaseConf.uri);
+      const client = await MongoClient.connect('mongodb://mongodb:27017');
       return new MongoConnection(databaseConf.name, client);
     },
   },
@@ -17,23 +17,17 @@ export const connections: Provider[] = [
     provide: 'QUEUE_CONNECTION',
     inject: ['QUEUE_CONFIGURATION'],
     useFactory: async (queueConf: any) => {
-      try {
-        console.log(queueConf);
-        const connection = await connect('amqp://guest:guest@rabbitmq:5672');
-        const channel = await connection.createConfirmChannel();
-        await channel.prefetch(1);
-        console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHSDJKAHGDF');
-        return new RabbitMQConnection(channel);
-      } catch (error) {
-        console.log(error);
-      }
+      const connection = await connect('amqp://guest:guest@rabbitmq:5672');
+      const channel = await connection.createConfirmChannel();
+      await channel.prefetch(1);
+      return new RabbitMQConnection(channel);
     },
   },
   {
     provide: 'CACHE_CONNECTION',
     inject: ['CACHE_CONFIGURATION'],
     useFactory: async (cacheConf: any) => {
-      const client = createClient(cacheConf);
+      const client = createClient({ url: 'redis://redis:6379' });
       await client.connect();
       return new RedisConnection(client);
     },
